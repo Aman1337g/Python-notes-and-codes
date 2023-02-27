b6cfa7f9efe7ad6f1a4a75d45e3d797639d3ad00
Aman Kumar Gupta <aman65144kumar@gmail.com>
2023-02-27 23:31:42 +0530

    Added multiplication table code

--- 11_For-Loops-in-Python.py
+++ 11_For-Loops-in-Python.py
@@ -82,3 +82,24 @@ OUTPUT
 
 1 3 5 7 9 11
 '''
+
+# printing multiplication table of user input value
+n = int(input("Enter a number : "))
+for i in range(1, 11):
+    print(n, "x", i, "=", n*i)
+
+'''
+OUTPUT
+
+Enter a number : 7
+7 x 1 = 7
+7 x 2 = 14
+7 x 3 = 21
+7 x 4 = 28
+7 x 5 = 35
+7 x 6 = 42
+7 x 7 = 49
+7 x 8 = 56
+7 x 9 = 63
+7 x 10 = 70
+'''
